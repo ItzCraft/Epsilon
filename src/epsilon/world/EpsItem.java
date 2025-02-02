ec0867f2bcea02f28a337d4e065869226c53b67c
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-02 10:02:12 +0200

    Update EpsItem.java

--- src/epsilon/world/EpsItem.java
+++ src/epsilon/world/EpsItem.java
@@ -3,7 +3,7 @@ package epsilon.world;
 import mindustry.type.Item;
 import epsilon.world.meta.EpsStats;
 
-public class EpsItem extends Item {
+public class EpsItem extends Item{
   public float Infection;
   public EpsItem(String name, Color color){
     super(name);
@@ -13,6 +13,6 @@ public class EpsItem extends Item {
   @Override
   public void setStats(){
     super.setStats();
-    stats.addPercent(EpsStats.infection, infection);
+    stats.addPercent(EpsStats.infection, infection); 
   }
 }
