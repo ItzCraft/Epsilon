4d48f1ef1c4171053bc5aaf34eec752a047e2ebe
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-02 09:57:50 +0200

    Update EpsStats.java

--- src/epsilon/world/meta/EpsStats.java
+++ src/epsilon/world/meta/EpsStats.java
@@ -3,7 +3,7 @@ package epsilon.world.meta;
 import mindustry.world.meta.Stat;
 
 public class EpsStats {
-  public static final EpsStats
+  public static final Stat
 
   infection = new Stat("infection");
 }
