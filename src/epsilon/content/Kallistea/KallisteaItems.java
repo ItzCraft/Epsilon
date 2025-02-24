f800a76330cd176dad828e67a6823ef867a711c2
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-24 17:17:07 +0200

    Update KallisteaItems.java

--- src/epsilon/content/Kallistea/KallisteaItems.java
+++ src/epsilon/content/Kallistea/KallisteaItems.java
@@ -2,6 +2,7 @@ package epsilon.content.Kallistea;
 
 import arc.graphics.*;
 import mindustry.type.Item;
+import epsilon.world.
 
 public class KallisteaItems {
     public static Item
@@ -14,8 +15,18 @@ public class KallisteaItems {
             charge = 0.7f;
             cost = 0.65f;
         }};
-        Calcite = new Item("calcite", Color.valueOf()){{
-            
+        Calcite = new Item("calcite", Color.valueOf("#b59d3c")){{
+            flammability = 0.3f;
+            cost = 0.85f;
+            hardness = 1;
+        }};
+        Quartz = new Item("quartz", Color.valueOf("#87b6c9")){{
+            charge = 0.2f;
+            hardness = 2;
+        }};
+        Fylion = new Item("fylion", Color.valueOf("56c772")){{
+            charge = 0.13f;
+            flammability = 0.19f;
         }};
     }
 }
