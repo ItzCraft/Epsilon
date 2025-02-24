e8152c7848ea4e8e7912b7c4082102a0d169e413
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-24 17:58:10 +0200

    Update KallisteaItems.java

--- src/epsilon/content/Kallistea/KallisteaItems.java
+++ src/epsilon/content/Kallistea/KallisteaItems.java
@@ -7,26 +7,26 @@ import epsilon.world.EpsItem;
 
 public class KallisteaItems {
     public static Item
-    Gelionyte, Calcite, Quartz, Fylion, redSand, Magnetite, Tantalum, Wierdium;
+    gelionyte, calcite, quartz, fylion, redSand, magnetite, tantalum, wierdium;
 
     public static final Seq<Item> kallisteaItems = new Seq<>();
 
     public static void load(){
-        Gelionyte = new Item("gelionyte", Color.valueOf("#e675ab")){{
+        gelionyte = new Item("gelionyte", Color.valueOf("#e675ab")){{
             charge = 0.7f;
             cost = 0.65f;
             hardness = 4;
         }};
-        Calcite = new Item("calcite", Color.valueOf("#b59d3c")){{
+        calcite = new Item("calcite", Color.valueOf("#b59d3c")){{
             flammability = 0.3f;
             cost = 0.85f;
             hardness = 1;
         }};
-        Quartz = new Item("quartz", Color.valueOf("#87b6c9")){{
+        quartz = new Item("quartz", Color.valueOf("#87b6c9")){{
             charge = 0.2f;
             hardness = 2;
         }};
-        Fylion = new Item("fylion", Color.valueOf("56c772")){{
+        fylion = new Item("fylion", Color.valueOf("56c772")){{
             charge = 0.13f;
             flammability = 0.19f;
         }};
@@ -34,5 +34,25 @@ public class KallisteaItems {
             flammability = 0.1f;
             hardness = 2;
         }};
+        magnetite = new EpsItem("magnetite", Color.valueOf("#346391")){{
+            explosiveness = 0.4f;
+            radioactivity = 0.05f;
+            hardness = 3;
+            infection = 0.7f;
+        }};
+        tantalum = new EpsItem("tantalum", Color.valueOf("8d60cc")){{
+            charge = 0.05f;
+            flammability = 0.08f;
+            explosiveness = 0.2f;
+            cost = 0.75f;
+            infection = 0.2f;
+        }};
+        wierdium = Item("wierdium", Color.valueOf("82b394")){{
+            charge = 0.07f;
+            flammability = 0.24f;
+            explosiveness = 0.45f;
+        }};
+
+        kallisteaItems.addAll(gelionyte, calcite, quartz, fylion, redSand, magnetite, tantalum, wierdium);
     }
 }
