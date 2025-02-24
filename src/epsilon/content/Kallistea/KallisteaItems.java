2f0f9bbdfbb21ba75d9c1ad132157e07fc310407
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-24 17:06:56 +0200

    Update KallisteaItems.java

--- src/epsilon/content/Kallistea/KallisteaItems.java
+++ src/epsilon/content/Kallistea/KallisteaItems.java
@@ -1,7 +1,17 @@
 package epsilon.content.Kallistea;
 
+import arc.graphics.*;
 import mindustry.type.Item;
 
 public class KallisteaItems {
     public static Item
+    Gelionyte, Calcite, Quartz, Fylion, Red sand, Magnetite, Tantalum, Wierdium;
+
+    public static final Seq<Item> kallisteaItems = new Seq<>()
+
+    public static void load(){
+        Gelionyte = new Item("gelionyte", Color.valueOf("#e675ab")){{
+            charge = 0.7f;
+        }};
+    }
 }
