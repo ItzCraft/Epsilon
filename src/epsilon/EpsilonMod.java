46377de1b539d5c07d5c811c3b27d951486444c3
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-24 21:17:11 +0200

    Update EpsilonMod.java

--- src/epsilon/EpsilonMod.java
+++ src/epsilon/EpsilonMod.java
@@ -17,17 +17,15 @@ public class EpsilonMod extends Mod{
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
-       /* Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
+          Events.on(ClientLoadEvent.class, e -> {
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                BaseDialog dialog = new BaseDialog("Epsilon");
+                dialog.cont.add(Core.bundle.get("warn-text1")).row();
+dialog.cont.image(Core.atlas.find("epsilon-mod-frog")).pad(20f).row();
+                dialog.cont.button("OK", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
-        });*/
+        });
     }
 
     @Override
