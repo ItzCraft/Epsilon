ec650cd8fd48fd88a1b0a1192568e3563fa13645
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-02-25 19:23:11 +0300

    item-

--- src/epsilon/content/Kallistea/KallisteaItems.java
+++ src/epsilon/content/Kallistea/KallisteaItems.java
@@ -12,42 +12,42 @@ public class KallisteaItems {
     public static final Seq<Item> kallisteaItems = new Seq<>();
 
     public static void load(){
-        gelionyte = new Item("gelionyte", Color.valueOf("#e675ab")){{
+        gelionyte = new Item("item-gelionyte", Color.valueOf("#e675ab")){{
             charge = 0.7f;
             cost = 0.65f;
             hardness = 4;
         }};
-        calcite = new Item("calcite", Color.valueOf("#b59d3c")){{
+        calcite = new Item("item-calcite", Color.valueOf("#b59d3c")){{
             flammability = 0.3f;
             cost = 0.85f;
             hardness = 1;
         }};
-        quartz = new Item("quartz", Color.valueOf("#87b6c9")){{
+        quartz = new Item("item-quartz", Color.valueOf("#87b6c9")){{
             charge = 0.2f;
             hardness = 2;
         }};
-        fylion = new Item("fylion", Color.valueOf("56c772")){{
+        fylion = new Item("item-fylion", Color.valueOf("56c772")){{
             charge = 0.13f;
             flammability = 0.19f;
         }};
-        redSand = new Item("red-sand", Color.valueOf("#c76a28")){{
+        redSand = new Item("item-red-sand", Color.valueOf("#c76a28")){{
             flammability = 0.1f;
             hardness = 2;
         }};
-        magnetite = new EpsItem("magnetite", Color.valueOf("#346391")){{
+        magnetite = new EpsItem("item-magnetite", Color.valueOf("#346391")){{
             explosiveness = 0.4f;
             radioactivity = 0.05f;
             hardness = 3;
             infection = 0.7f;
         }};
-        tantalum = new EpsItem("tantalum", Color.valueOf("#8d60cc")){{
+        tantalum = new EpsItem("item-tantalum", Color.valueOf("#8d60cc")){{
             charge = 0.05f;
             flammability = 0.08f;
             explosiveness = 0.2f;
             cost = 0.75f;
             infection = 0.2f;
         }};
-        wierdium = new Item("wierdium", Color.valueOf("#82b394")){{
+        wierdium = new Item("item-wierdium", Color.valueOf("#82b394")){{
             charge = 0.07f;
             flammability = 0.24f;
             explosiveness = 0.45f;
