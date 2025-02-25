210c2b2aa009dacdd5794fb22dca9ce2e71d2baf
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-02-25 07:45:24 +0200

    it won't gonna work frfr

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -2,7 +2,26 @@ package epsilon.ui.dialogs;
 
 import mindustry.ui.dialogs.BaseDialog;
 
-/* public class AboutEpsilonDialog extends BaseDialog{
+public class AboutEpsilonDialog extends BaseDialog{
 
-} */
-// later I'm gonna continue
+    public BaseDialog EpsDataBase;
+
+    public AboutEpsilonDialog(){
+        super(@about.epsilon);
+        addCloseButton();
+        shouldPause = true;
+
+        EpsDataBase = new BaseDialog(){
+            EpsDataBase.addCloseButton();
+                    dataDialog.cont.table(Tex.button, t -> {
+            t.defaults().size(280f, 60f).left();
+            TextButtonStyle style = Styles.flatt;
+
+                t.button("@settings.epsilon-solar-system-database", "@epsilon-solar-system-icon", style, () -> {
+
+t.cont.add(Core.bundle.get("settings.epsilon-solar-system-text")).row();
+              t.cont.button(Core.bundle.get("settings.back"), dialog::hide).size(100f, 50f); 
+                }) ;
+        } 
+    } 
+}
