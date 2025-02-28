f4fb538ffaa82ef87dd7ed0b18139f62b37a8d68
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-02-28 22:32:53 +0300

    AboutEpsilonDialog was finished for now? (Ебал я этот код)

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -1,37 +1,11 @@
 package epsilon.ui.dialogs;
 
-import arc.Core;
-import arc.scene.ui.layout.Table;
-import mindustry.gen.Tex;
-import mindustry.ui.*;
 import mindustry.ui.dialogs.BaseDialog;
 
-<<<<<<< HEAD
-import static arc.input.KeyCode.t;
-
 public class AboutEpsilonDialog extends BaseDialog {
-=======
-/* public class AboutEpsilonDialog extends BaseDialog{
->>>>>>> 8997e41ab32b93ff9cafe50da16bdd29709ddc58
-
-    public BaseDialog EpsDataBase;
-
     public AboutEpsilonDialog(){
         super("@about.epsilon");
         addCloseButton();
         shouldPause = true;
-        Table table = new Table();
-        table.button(Core.bundle.get("@bebe"), () -> {
-            table.add(Core.bundle.get("@bebebe"));
-        });
-
-<<<<<<< HEAD
     };
 }
-=======
-        EpsDataBase = new BaseDialog("Epsilon Database"){
-            
-        };
-    } 
-} */
->>>>>>> 8997e41ab32b93ff9cafe50da16bdd29709ddc58
