468354c3afe48d3f54f033f595acfade320c19f4
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-02-28 22:32:18 +0300

    Create EpsilonVars.java

---
+++ src/epsilon/EpsilonVars.java
@@ -0,0 +1,8 @@
+package epsilon;
+
+import arc.Core;
+
+public class EpsilonVars{
+    //will the warn dialog be displayed
+    public static boolean  hideWarnDialog = Core.settings.getBool("epsilon-hide-warn-dialog");
+}
