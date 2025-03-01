edcd928c54d15875c4bac3e84f6539f0cd64a00b
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 18:06:57 +0200

    Create BreakableTallBlock.java

---
+++ src/epsilon/world/blocks/environment/BreakableTallBlock.java
@@ -0,0 +1,10 @@
+package epsilon.world.blocks.environment;
+
+import mindustry.world.blocks.environment.TallBlock;
+
+public class BreakableTallBlock extends TallBlock{
+      public BreakableTallBlock(String name){
+          super(name);
+          
+      }
+}
