5829f56c02055d6944b12734fa06a282008adc64
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 08:35:39 +0200

    Update KallisteaDistribution.java

--- src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
@@ -3,9 +3,10 @@ package epsilon.content.Kallistea.blocks;
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.*;
 import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import epsilon.content.Kallistea.KallisteaItems;
 
-import static mindustry.type.ItemStack.*;
+import static mindustry.type.ItemStack.with;
 
 public class KallisteaDistribution{
     public static Block
@@ -13,7 +14,7 @@ public class KallisteaDistribution{
 
     public static void load(){
         transmittingBridge = new DuctBridge("transmitting-bridge"){{
-            requirements(Category.distribution, ItemStack.with(KallisteaItems.calcite, 1));
+            requirements(Category.distribution, with(KallisteaItems.calcite, 1));
             size = 1;
             health = 20;
             speed = 2;
