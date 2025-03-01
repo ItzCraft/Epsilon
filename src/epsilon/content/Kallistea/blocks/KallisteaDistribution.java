190f79311c275c2187b9150b37ace685db21b444
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 08:21:11 +0200

    Update blocks

---
+++ src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
@@ -0,0 +1,9 @@
+package epsilon.content.Kallistea.blocks;
+
+import mindustry.world.Block;
+import mindustry.world.distribution.*;
+
+public class KallisteaDistribution{
+    public static Block
+    transmittingBridge;
+}
