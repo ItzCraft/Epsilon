4d956f6931feb90146c0cd055ee1d3004e0abc6f
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 21:41:35 +0200

    Update AboutEpsilonDialog.java

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -12,12 +12,12 @@ public class AboutEpsilonDialog extends BaseDialog {
         addCloseButton();
         shouldPause = true;
 
-        BaseDialog buttons = new BaseDialog()
+        BaseDialog buttons = new BaseDialog();
 
         buttons.addCloseButton();
 
         buttons.cont.button(Core.bundle.get("epsilon-database-ss-button"), () -> {
-            BaseDialog EpsilonSolarSystem = new BaseDialog("@epsilon-solar-system-title")
+            BaseDialog EpsilonSolarSystem = new BaseDialog("@epsilon-solar-system-title");
             EpsilonSolarSystem.cont.add(Core.bundle.get("epsilon-database-as-text1")).row();
         }).size(100f, 50f);
     };
