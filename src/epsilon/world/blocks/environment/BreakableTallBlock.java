14ef1d265b54255a6106af75319fea560a7abb7d
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 18:09:16 +0200

    Update BreakableTallBlock.java

--- src/epsilon/world/blocks/environment/BreakableTallBlock.java
+++ src/epsilon/world/blocks/environment/BreakableTallBlock.java
@@ -5,6 +5,8 @@ import mindustry.world.blocks.environment.TallBlock;
 public class BreakableTallBlock extends TallBlock{
       public BreakableTallBlock(String name){
           super(name);
-          
+          breakable = true;
+          breakEffect = Fx.breakProp;
+          breakSound = Sounds.rockBreak;
       }
 }
