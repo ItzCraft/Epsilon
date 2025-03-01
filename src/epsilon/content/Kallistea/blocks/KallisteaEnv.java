67b4a988938cc822e8cd85ecd7623f533e92abbb
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 18:26:23 +0200

    Update KallisteaEnv.java

--- src/epsilon/content/Kallistea/blocks/KallisteaEnv.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaEnv.java
@@ -4,7 +4,10 @@ import mindustry.world.Block;
 import mindustry.world.blocks.environment.*;
 import epsilon.content.Kallistea.KallisteaItems;
 import epsilon.world.blocks.environment.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 
+import static mindustry.type.ItemStack.with;
 import static epsilon.world.EpsAttribute.infection;
 
 
@@ -51,7 +54,7 @@ public class KallisteaEnv {
                 purystalBoulder = new Prop("purystal-boulder"){{variants = 4; customShadow = true;
                     purystalErodedFloor.asFloor().decoration = this; purystalFloor.asFloor().decoration = this; purystalLightFloor.asFloor().decoration = this;
                 }};
-                gelionyteCluster = new BreakableTallBlock("gelionyte-cluster"){{variants = 2; clipSize = 148; shadowOffset = -0.68f;}};
+                gelionyteCluster = new BreakableTallBlock("gelionyte-cluster"){{requirements(Category.distribution, with(KallisteaItems.gelionyte, 10)); variants = 2; clipSize = 148; shadowOffset = -0.68f;}};
                 buahanBush = new Prop("buahan-bush"){{variants = 3; customShadow = true;}};
                 buahanTree = new TreeBlock("buahan-tree"){{variants = 2; clipSize = 200; shadowOffset = -1.53f;}};
    
