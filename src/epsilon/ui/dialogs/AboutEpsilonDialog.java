ca95898df431a13d821773b0f3299317d2b382c0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 21:39:29 +0200

    Update AboutEpsilonDialog.java

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -1,11 +1,24 @@
 package epsilon.ui.dialogs;
 
+import arc.*;
+import arc.util.*;
 import mindustry.ui.dialogs.BaseDialog;
+
+import static arc.Core.bundle;
 
 public class AboutEpsilonDialog extends BaseDialog {
     public AboutEpsilonDialog(){
         super("@about.epsilon");
         addCloseButton();
         shouldPause = true;
+
+        BaseDialog buttons = new BaseDialog()
+
+        buttons.addCloseButton();
+
+        buttons.cont.button(Core.bundle.get("epsilon-database-ss-buttom), () -> {
+            BaseDialog EpsilonSolarSystem = new BaseDialog("@epsilon-solar-system-title")
+            EpsilonSolarSystem.cont.add(Core.bundle.get("epsilon-database-as-text1")).row();
+        }).size(100f, 50f);
     };
 }
