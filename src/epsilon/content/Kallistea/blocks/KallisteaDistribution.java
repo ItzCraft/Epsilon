720292d2c2009495fb9c5f36357e58bf638d68b0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 21:30:31 +0200

    Update KallisteaDistribution.java

--- src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
@@ -19,6 +19,7 @@ public class KallisteaDistribution{
             health = 20;
             speed = 2;
             itemCapacity = 3;
+            range = 2;
         }};
         transmittingJunction = new Junction("transmitting-junction"){{
             requirements(Category.distribution, with(KallisteaItems.calcite, 2));
@@ -26,5 +27,29 @@ public class KallisteaDistribution{
             health = 15;
             speed = 3;
         }};
+        transmittingRouter = new Router("transmitting-router"){{
+            requirements(Category.distribution, with(KallisteaItems.calcite, 2));
+            size = 1;
+            health = 15;
+            speed = 3;
+        }};
+        transmittingSorter = new Sorter("transmitting-sorter"){{
+            requirements(Category.distribution, with(KallisteaItems.calcite, 6));
+            size = 1;
+            health = 20;
+        }};
+        transmittingOverflowGate = new OverflowGate("transmitting-overflow-gate"){{
+            requirements(Category.distribution, with(KallisteaItems.calcite, 14));
+            size = 1;
+            health = 30;
+            speed = 1;
+        }};
+        transmittingUnderflowGate = new OverflowGate("transmitting-underflow-gate"){{
+            requirements(Category.distribution, with(KallisteaItems.calcite, 14));
+            size = 1;
+            health = 30;
+            speed = 1;
+            invert = true;
+        }}; 
     }
 }
