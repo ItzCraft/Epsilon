7cc5a252f51df22c94dbe3f54c1b66810e5f3f95
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-01 08:29:13 +0200

    Update KallisteaDistribution.java

--- src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaDistribution.java
@@ -2,8 +2,19 @@ package epsilon.content.Kallistea.blocks;
 
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.*;
+import epsilon.content.Kallistea.KallisteaItems;
 
 public class KallisteaDistribution{
     public static Block
-    transmittingBridge;
+    transmittingBridge, transmittingJunction, transmittingRouter, transmittingSorter, transmittingOverflowGate, transmittingUnderflowGate;
+
+    public static void load(){
+        transmittingBridge = new DuctBridge("transmitting-bridge"){{
+            requirements(Category.distribution, ItemStack.with(KallisteaItems.calcite, 1);
+            size = 1;
+            health = 20;
+            speed = 2;
+            itemCapacity = 3;
+        }};
+    }
 }
