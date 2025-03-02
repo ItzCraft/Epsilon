940c4536c77dab4006c1ff9eeed5d54c17184360
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-02 20:28:01 +0200

    Update AboutEpsilonDialog.java

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -24,14 +24,14 @@ public class AboutEpsilonDialog extends BaseDialog {
             EpsilonSolarSystem.addCloseButton();
             EpsilonSolarSystem.cont.add(Core.bundle.get("epsilon-database-as-text1")).row();
             EpsilonSolarSystem.show();
-        }).growY().size(195f, 90f);
+        }).growY().center().size(195f, 90f);
 
         table.button(Core.bundle.get("epsilon-database-fenspor-button"), () -> {
             BaseDialog EpsilonSolarSystem = new BaseDialog("@epsilon-solar-system-title");
             EpsilonSolarSystem.addCloseButton();
             EpsilonSolarSystem.cont.add(Core.bundle.get("epsilon-database-as-text1")).row();
             EpsilonSolarSystem.show();
-        }).growY().padTop(190f).size(195f, 90f);
+        }).growY().center().padTop(190f).size(195f, 90f);
         cont.add(table);
     }
 }
