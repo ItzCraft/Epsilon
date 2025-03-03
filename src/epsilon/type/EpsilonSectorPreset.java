727e17ea4b8a1e348fd1446391b532123a7c38c2
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-03 11:00:29 +0200

    Update EpsilonSectorPreset.java

--- src/epsilon/type/EpsilonSectorPreset.java
+++ src/epsilon/type/EpsilonSectorPreset.java
@@ -7,11 +7,17 @@ import mindustry.type.*;
 
 public class EpsilonSectorPreset extends SectorPreset{
     public TextureRegion icon;
+    public Sector sector
     public EpsilonSectorPreset(String name, Planet planet, int sector){
         super(name, planet, sector);
     }
 
     public void loadIcon(){
         uiIcon = fullIcon = icon;
+    }
+
+    @Override
+    public ContentType getContentType(){
+        return ContentType.sector;
     }
 }
