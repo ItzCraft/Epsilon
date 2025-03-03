95bed118ef91edc962ea2d35fbffdfb37caeb8bf
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-03 11:08:15 +0200

    Update EpsilonSector.java

--- src/epsilon/type/EpsilonSector.java
+++ src/epsilon/type/EpsilonSector.java
@@ -3,5 +3,13 @@ package epsilon.type;
 import mindustry.type.*;
 
 /* public class EpsilonSector extends Sector{
+    public Sector(Planet planet, Ptile tile){
+        super(planet, tile);
+    }
 
+    // idk if it's gonna work tbh
+    @Override
+    public String displayThreat(){
+                String[] threats = {"none", "low", "medium", "high", "extreme", "eradication"};
+    }
 } */
