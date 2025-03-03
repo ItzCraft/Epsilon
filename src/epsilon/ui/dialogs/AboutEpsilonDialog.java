111d43f21572874313240b9922b9a42145fc7e77
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-03-03 21:35:47 +0300

    CODING STYLE? YES, ITZCRAFT!???

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -7,7 +7,7 @@ import mindustry.ui.dialogs.BaseDialog;
 
 import static arc.Core.bundle;
 
-public class AboutEpsilonDialog extends BaseDialog {
+public class AboutEpsilonDialog extends BaseDialog{
     public AboutEpsilonDialog(){
         super("@about.epsilon");
         addCloseButton();
