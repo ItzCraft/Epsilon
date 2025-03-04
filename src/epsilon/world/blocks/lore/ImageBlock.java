4a38f37be9bfe8e43349a92af3eb93a1b9b93d58
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-04 10:32:07 +0200

    Create ImageBlock.java

---
+++ src/epsilon/world/blocks/lore/ImageBlock.java
@@ -0,0 +1,21 @@
+package epsilon.world.blocks.lore;
+
+import mindsutry.world.Block;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class ImageBlock extends Block{
+    // idk If it gonna work with Textureregion
+    public String image;
+    public ImageBlock(String name){
+        super(name);
+        update = true;
+        breakable = false;
+    }
+
+    /* public class ImageBlockBuild extends Building{
+        @Override
+        public void buildConfiguration(Table table){
+            
+        } 
+    } */
+}
