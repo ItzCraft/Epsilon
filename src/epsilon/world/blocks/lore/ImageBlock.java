4688b8e3c2c1d5cc7025b020361bfb4aa3ed80b5
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-03-04 12:57:40 +0300

    Fixed imports for ImageBlock

--- src/epsilon/world/blocks/lore/ImageBlock.java
+++ src/epsilon/world/blocks/lore/ImageBlock.java
@@ -1,6 +1,10 @@
 package epsilon.world.blocks.lore;
 
+import arc.Core;
 import arc.scene.ui.layout.Table;
+import mindustry.gen.Building;
+import mindustry.gen.Icon;
+import mindustry.ui.Styles;
 import mindustry.world.Block;
 import mindustry.ui.dialogs.BaseDialog;
 
