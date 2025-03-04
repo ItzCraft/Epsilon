170f82e0c7f229abe5bee34c2c5e01e962807431
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-04 19:46:44 +0200

    Update ConfigurableDrill.java

--- src/epsilon/world/blocks/production/ConfigurableDrill.java
+++ src/epsilon/world/blocks/production/ConfigurableDrill.java
@@ -13,15 +13,42 @@ public class ConfigurableDrill extends Drill{
         saveConfig = true;
     }
     /*public class ConfigurableDrillBuild extends DrillBuild{
+        public boolean eff1 = false;
+        public boolean eff2 = true;
+        public boolean eff3 = false;
+        public final int oldDrillTime = drillTime;
+
         @Override 
         public void updateTile(){
-            
+            if(eff1){
+                drillTime = oldDrillTime * efficiency1;
+            } else if(eff2){
+                drillTime = oldDrillTime * efficiency2;
+            } else{
+                drillTime = oldDrillTime * efficiency3;
+            }
+            super.updateTile();
         }
 
         @Override
         public void buildConfiguration(Table table){
-            table.button(ToString(efficiency1) + "x", Styles.cleari, () -> {
-                
+            table.button(efficiency1.toString() + "x", Styles.cleari, () -> {
+                eff1 = true;
+                eff2 = false;
+                eff3 = false;
+                deselect();
+            });
+           table.button(efficiency2.toString() + "x", Styles.cleari, () -> {
+                eff1 = false;
+                eff2 = true;
+                eff3 = false;
+                deselect();
+            });
+            table.button(efficiency3.toString() + "x", Styles.cleari, () -> {
+                eff1 = false;
+                eff2 = false;
+                eff3 = true;
+                deselect();
             });
         }
     }*/
