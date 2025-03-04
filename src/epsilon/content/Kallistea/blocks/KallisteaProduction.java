21c03f7357cc9c6b1333971cced5151cb4237420
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-04 16:30:13 +0200

    Create KallisteaProduction.java

---
+++ src/epsilon/content/Kallistea/blocks/KallisteaProduction.java
@@ -0,0 +1,24 @@
+package epsilon.content.Kallistea.blocks;
+
+import mindustry.world.Block;
+import mindustry.world.production.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import epsilon.content.Kallistea.KallisteaItems;
+
+import static mindustry.type.ItemStack.with;
+
+public class KallisteaProduction{
+    public static Block
+    exponentialDrill;
+
+    public static void load(){
+        exponentialDrill = new Drill("exponential-drill"){{
+            requirements(Category.production, with(KallisteaItems.gelionyte, 25, KallisteaItems.calcite, 20));
+            health = 120;
+            size = 3;
+            drillTime = 320;
+            tier = 3;
+        }};
+    } 
+}
