72940f7d9bbf08a79bb6ecb50c64d32d38baaf32
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-05 23:29:43 +0200

    Update EpsUi.java

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -5,6 +5,13 @@ import mindustry.ui.*;
 
 /*public class EpsUi extends Ui{
     public void showDialog(Textureregion unitIcon, String text, float duration){
-
+         var cinfo = Core.scene.find("coreinfo");
+        Table table = new Table();
+        table.touchable = Touchable.disabled;
+        table.setFillParent(true);
+        if(cinfo.visible && !state.isMenu()) table.marginTop(cinfo.getPrefHeight() / Scl.scl() / 2);
+                table.actions(Actions.delay(duration * 0.9f), Actions.fadeOut(duration * 0.1f, Interp.fade), Actions.remove());
+        // table ui code itself soon
+        Core.scene.add(table);
     }
 } */
