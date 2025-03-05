979131cdf2da65a0121efd610b702df82d296d39
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-05 09:35:55 +0200

    Update EpsStats.java

--- src/epsilon/world/meta/EpsStats.java
+++ src/epsilon/world/meta/EpsStats.java
@@ -1,8 +1,12 @@
 package epsilon.world.meta;
 
 import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatCat;
 
 public class EpsStats {
   public static final Stat
-          infection = new Stat("infection");
+          infection = new Stat("infection"),
+          efficiency1 = new Stat("efficiency-1", StatCat.general);
+          efficiency2 = new Stat("efficiency-2", StatCat.general); 
+          efficiency3 = new Stat("efficiency-3", StatCat.general);
 }
