ee171c9ac034e533bfa29868895b69fe69b890d0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 15:20:33 +0200

    Update DialogStatements.java

--- src/epsilon/logic/DialogStatements.java
+++ src/epsilon/logic/DialogStatements.java
@@ -6,25 +6,10 @@ import arc.scene.ui.layout.*;
 import mindustry.logic.*;
 import mindustry.logic.LExecutor.*;
 import mindustry.ui.*;
-//import epsilon.logic.DialogLInstuctions.*;
+import epsilon.logic.DialogLInstuctions.*;
 
-/*public class DialogStatements{
-    // Code from Mechanical Arms mod by MinRi2
-    private static final Seq<Prov<DialogLStatement>> dialogAllStatements = Seq.with(
-    ControlSwitchStatement::new
-    );
-    
-    
-    public static void register(){
-        Seq<Prov<DialogLStatement>> seq = dialogAllStatements.map(prov -> prov::get);
-        LogicIO.allStatements.addAll(seq);
-
- for(Prov<DialogLStatement> prov : dialogAllStatements){
-            DialogLStatement example = prov.get();
-            LAssembler.customParsers.put(example.markName, example::read);
-        }
-    }
-
+public class DialogStatements{
+    @RegisterStatement("dialog-control")
     public static class DialogControlStatement extends LStatement{
         public LVar duration = 5;
         public String unitIconName = "epsilon-assets/frog.png";
@@ -49,4 +34,4 @@ import mindustry.ui.*;
             fields(table, duration, str -> duration = str);
             table.add(" sec ");
     }
-}*/
+}
