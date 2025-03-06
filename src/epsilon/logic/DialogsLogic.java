f34ca3aa42b8fcf6fc761b326f6ac53bcc8f6eab
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 16:34:06 +0200

    Update DialogsLogic.java

--- src/epsilon/logic/DialogsLogic.java
+++ src/epsilon/logic/DialogsLogic.java
@@ -4,11 +4,10 @@ import arc.graphics.*;
 import mindustry.logic.*;
 import epsilon.*;
 
-/*public class DialogsLogic{
+public class DialogsLogic{
     public static LCategory dialogCategory;
 
     public static void init(){
         dialogCategory = new LCategory("Dialogs", Color.valueOf("eb347d"));
-        DialogStatements.register();
     } 
-}*/
+}
