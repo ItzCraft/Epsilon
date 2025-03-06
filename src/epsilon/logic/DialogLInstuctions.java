59889f4340e8ea68edf96569b13c9db6708b55c1
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 06:32:25 +0200

    i have noidea if this works

--- src/epsilon/logic/DialogLInstuctions.java
+++ src/epsilon/logic/DialogLInstuctions.java
@@ -2,7 +2,61 @@ package epsilon.logic;
 
 import mindustry.logic.*;
 import mindustry.logic.LExecutor.*;
+// import epsilon.ui.EpsUi;
 
 /*public class DialogLInstuctions{
-    
+    public static abstract class DialogControlInstruction implements LInstruction{
+        public LVar duration;
+        public String unitIconName;
+
+        public DialogControlInstruction(LVar duration, String unitIconName){
+            this.unitIconName = unitIconName;
+            this.duration = duration;
+            return this;
+        }
+        
+        @Override
+        public void run(LExecutor exec){
+            String text = exec.textBuffer.toString();
+            if(text.startsWith("@")){
+                String substr = text.substring(1);
+                if(Core.bundle.has(substr)){
+                    text = Core.bundle.get(substr);
+                }
+            }
+
+            EpsUi.showDialog(unitIconName, text, duration.numf());
+        } 
+    }
+
+    public static abstract class BaseDialogTextInstruction implements LInstruction{
+        public String name = template;
+        public LVar duration;
+
+        public BaseDialogTextInstruction(String name, LVar duration){
+            this.name = name;
+            this.duration = duration
+            return this;
+        }
+
+        @Override 
+        public void run(LExecutor exec){
+            String text = exec.textBuffer.toString();
+            if(text.startsWith("@")){
+                String substr = text.substring(1);
+                if(Core.bundle.has(substr)){
+                    text = Core.bundle.get(substr);
+                }
+            }
+
+            if(name.startsWith("@")){
+                String subname = name.substring(1)
+                if(Core.bundle.has(subname)){
+                    name = Core.bundle.get(subname);
+                }
+            }
+
+            EpsUi.BaseDialogText(name, text, duration.numf());
+        }
+    }
 }*/
