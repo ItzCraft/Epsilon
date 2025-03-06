d8783a6996ef908d5ef009359fa644ca558ffb61
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 20:51:11 +0200

    Create DialogBlock.java

---
+++ src/epsilon/world/blocks/lore/DialogBlock.java
@@ -0,0 +1,26 @@
+package epsilon.world.blocks.lore;
+
+import arc.Core;
+inport arc.scene.ui.layout.Table;
+import mindustry.gen.Building;
+import mindustry.world.Block;
+import epsilon.ui.EpsUi;
+
+// This block is a another method of showing Dialogs for that case, we won't make it in wprocs
+public class DialogBlock extends Block{
+    public int duration = 5;
+    public String unitIconName = "epsilon-frog";
+    public String text = "Template";
+
+    public DialogBlock(String name){
+        super(name);
+        destructible = false;
+    }
+
+    
+    @Override
+    public void init(){
+        super.init();
+        EpsUi.showDialog(unitIconName, text, duration);
+    }
+}
