771ae59ed6311e1e9cd4946da696d91a1429db8e
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 03:59:07 +0200

    Update EpsUi.java

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -1,7 +1,7 @@
 package epsilon.ui;
 
 import arc.scene.ui.layout.Table;
-import mindustry.ui.*;
+import mindustry.ui.*
 
 /*public class EpsUi extends Ui{
     public void showDialog(String unitIconName, String text, float duration){
@@ -10,10 +10,10 @@ import mindustry.ui.*;
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
         if(cinfo.visible && !state.isMenu()) table.marginTop(cinfo.getPrefHeight() / Scl.scl() / 2);
-        table.actions(Actions.delay(duration * 0.9f), Actions.fadeOut(duration * 0.1f, Interp.fade), Actions.remove());
+        table.actions(Actions.delay(duration * 1.2f), Actions.fadeOut(duration * 0.2f, Interp.fade), Actions.remove());
         table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(10f);
         // wanna make it through Flabel but idk how
-        table.bottom().table(Styles. black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(10f);
+        table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(10f);
         Core.scene.add(table);
     }
 } */
