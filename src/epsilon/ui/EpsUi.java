f26a2bd17832fafff4c536a3497029d881dabc2b
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 04:03:31 +0200

    Update EpsUi.java

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -3,7 +3,7 @@ package epsilon.ui;
 import arc.scene.ui.layout.Table;
 import mindustry.ui.*;
 
-/*public class EpsUi extends Ui{
+public class EpsUi extends Ui{
     public void showDialog(String unitIconName, String text, float duration){
          var cinfo = Core.scene.find("coreinfo");
         Table table = new Table();
@@ -16,4 +16,4 @@ import mindustry.ui.*;
         table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(10f);
         Core.scene.add(table);
     }
-} */
+}
