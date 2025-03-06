bae6fd3ee76cf12a785b6067afef30f559abb6f0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 04:15:27 +0200

    Create DialogsLogic.java

---
+++ src/epsilon/logic/DialogsLogic.java
@@ -0,0 +1,14 @@
+package epsilon.logic;
+
+import arc.graphics.*;
+import mindustry.logic.*;
+import epsilon.*;
+
+/*public class DialogsLogic{
+    public static LCategory dialogCategory;
+
+    public static void load(){
+        dialogCategory = new LCategory("Dialogs", Color.valueOf("eb347d"));
+        dialogStatements.register();
+    } 
+}*/
