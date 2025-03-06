2a87ff08c4b534af2aa8508935adfea160f6c98a
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 09:56:06 +0200

    Update EpsUi.java

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -9,7 +9,7 @@ import static arc.Core.bundle;
 import static mindustry.Vars.ui;
 import static arc.scene.actions.Actions.*;
 
-public class EpsUi extends Ui{
+public class EpsUi extends UI{
     public void showDialog(String unitIconName, String text, float duration){
         Table table = new Table();
         table.touchable = Touchable.disabled;
