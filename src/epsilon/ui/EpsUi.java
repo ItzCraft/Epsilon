16375b3c594b33518c7b78a31babd83dc74d76da
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 09:50:07 +0200

    Update EpsUi.java

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -11,11 +11,9 @@ import static arc.scene.actions.Actions.*;
 
 public class EpsUi extends Ui{
     public void showDialog(String unitIconName, String text, float duration){
-         var cinfo = Core.scene.find("coreinfo");
         Table table = new Table();
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
-        if(cinfo.visible && !state.isMenu()) table.marginTop(cinfo.getPrefHeight() / Scl.scl() / 2);
         table.actions(Actions.delay(duration * 1.2f), Actions.fadeOut(duration * 0.2f, Interp.fade), Actions.remove());
         table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(10f);
         // wanna make it through Flabel but idk how
