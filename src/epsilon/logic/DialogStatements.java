71308e48fd2a80a1aa7244c7287683ec1bc60eb2
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 17:27:59 +0200

    Update DialogStatements.java

--- src/epsilon/logic/DialogStatements.java
+++ src/epsilon/logic/DialogStatements.java
@@ -10,7 +10,7 @@ import epsilon.logic.DialogLInstuctions.*;
 
 public class DialogStatements{
     public static class DialogControlStatement extends LStatement{
-        public String duration= "5";
+        public int duration = 5;
         public String unitIconName = "epsilon-frog";
 
         @Override
@@ -35,7 +35,7 @@ public class DialogStatements{
             fields(table, unitIconName, str -> unitIconName = str);
             table.row();
             table.add(" for ");
-            fields(table, duration, str -> duration = str);
+            fields(table, duration, int -> duration = int);
             table.add(" sec ");
         }
     }
