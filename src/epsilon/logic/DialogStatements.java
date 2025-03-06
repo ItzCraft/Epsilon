7cf614c32ddc6a54112a6354151cdf5b0ba44abd
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 04:24:22 +0200

    Create DialogStatements.java

---
+++ src/epsilon/logic/DialogStatements.java
@@ -0,0 +1,12 @@
+package epsilon.logic;
+
+import arc.scene.style.*;
+import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
+import mindustry.logic.*;
+import mindustry.logic.LExecutor.*;
+import mindustry.ui.*;
+
+/*public class DialogStatements{
+    
+}*/
