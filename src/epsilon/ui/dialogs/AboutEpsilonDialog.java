3babf68474fa246fa3a9ee3b5fcb1a70a839d69a
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 10:17:43 +0200

    For tests

--- src/epsilon/ui/dialogs/AboutEpsilonDialog.java
+++ src/epsilon/ui/dialogs/AboutEpsilonDialog.java
@@ -7,6 +7,8 @@ import mindustry.ui.dialogs.BaseDialog;
 
 import static arc.Core.bundle;
 
+import epsilon.ui.EpsUi;
+
 public class AboutEpsilonDialog extends BaseDialog{
     public AboutEpsilonDialog(){
         super("@about.epsilon");
@@ -27,10 +29,7 @@ public class AboutEpsilonDialog extends BaseDialog{
         }).padTop(-150).pad(-75).size(195f, 90f);
 
         table.button(Core.bundle.get("epsilon-database-fenspor-button"), () -> {
-            BaseDialog EpsilonSolarSystem = new BaseDialog("@epsilon-solar-system-title");
-            EpsilonSolarSystem.addCloseButton();
-            EpsilonSolarSystem.cont.add(Core.bundle.get("epsilon-database-as-text1")).row();
-            EpsilonSolarSystem.show();
+            EpsUi.showDialog("epsilon-assets/frog.png", "blah blah", 10);
         }).padTop(100).size(195f, 90f);
         cont.add(table).width(500f).height(100f);
     }
