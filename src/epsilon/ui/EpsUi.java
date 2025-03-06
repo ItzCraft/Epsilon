343045964f4b90a40cb642bb03dd10bc2194c2df
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-06 11:03:47 +0200

    Final addition

--- src/epsilon/ui/EpsUi.java
+++ src/epsilon/ui/EpsUi.java
@@ -17,7 +17,7 @@ public class EpsUi extends UI{
         Table table = new Table();
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
-        table.actions(Actions.delay(duration * 1.2f), Actions.fadeOut(duration * 0.6f, Interp.fade), Actions.remove());
+        table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
         table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
         // wanna make it through Flabel but idk how
         table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(130f, 60f);
