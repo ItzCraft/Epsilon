8ac926b58df6e64628009dee69172e9c90bbe6c1
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 13:06:49 +0200

    Update Cutscene.java

--- src/epsilon/cutscenes/Cutscene.java
+++ src/epsilon/cutscenes/Cutscene.java
@@ -5,7 +5,7 @@ import arc.struct.Seq;
 import static mindustry.Vars.ui;
 
 public class Cutscene{
-    /*public static phaseCutscene(String tokens){
+    public static phaseCutscene(String tokens){
          Seq<String> tokensArray = parseString(tokens);
          String actionName = tokensArray.remove(0);
          String[] args = tokensArray.toArray(String.class);
@@ -19,5 +19,5 @@ public class Cutscene{
             ui.announce("Failed to parse action: " + tokens);
             return new CutsceneNull();
          }
-    } */
+    }
 }
