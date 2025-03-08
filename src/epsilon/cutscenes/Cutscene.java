66129c9c6fa4cbca9be588ed8b85b72e7e9f165d
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 13:26:41 +0200

    Update Cutscene.java

--- src/epsilon/cutscenes/Cutscene.java
+++ src/epsilon/cutscenes/Cutscene.java
@@ -2,6 +2,7 @@ package epsilon.cutscene;
 
 import arc.struct.Seq;
 import mindustry.gen.Building;
+import epsilon.cutscene.TextShowup;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
