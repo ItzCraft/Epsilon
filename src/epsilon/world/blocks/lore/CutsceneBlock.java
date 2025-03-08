f85f51ad94332f33d66ee901889243a97cbae3a7
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 13:46:00 +0200

    Create CutsceneBlock.java

---
+++ src/epsilon/world/blocks/lore/CutsceneBlock.java
@@ -0,0 +1,78 @@
+package epsilon.world.blocks.lore;
+
+import arc.scene.ui.layout.Table;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.logic.LAccess;
+import mindustry.type.Category;
+import mindustry.world.blocks.logic.MessageBlock;
+
+import epsilon.cutscenes.Cutscene;
+
+import static mindustry.Vars.ui;
+
+public class CutsceneBlock extends MessageBlock{
+    public CutsceneBlock(String name){
+        super(name);
+        maxTextLength = 5000;
+        maxNewlines = 200;
+        configurable = true;
+        destructible = true;
+        solid = true;
+        update = true;
+        noUpdateDisabled = false;
+        category = Category.logic;
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    public class CutsceneControllerBuild extends MessageBuild implements CutsceneTrigger{
+        @Override
+        public void drawSelect(){}
+
+        @Override
+        public void buildConfiguration(Table table){
+            super.buildConfiguration(table);
+            table.button(Icon.play, Styles.cleari, this::activate).size(40f);
+        }
+
+        @Override
+        public void created(){
+            deactivate();
+        }
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
+            if (enabled) {
+                playCutscene();
+                enabled = false;
+            }
+        }
+        public void playCutscene(){
+            try{
+                cutscene.addSubActionBus(Cutscene.phaseCode(message.toString(), this));
+            }catch (Exception e){
+                ui.announce("Failed to create cutscene in block: " + tileX() + " " + tileY());
+            }
+        }
+        @Override
+        public void activate() {
+            enabled = true;
+        }
+
+        @Override
+        public void deactivate() {
+            enabled = false;
+        }
+
+        @Override
+        public void write(Writes write) {
+            super.write(write);
+        }
+
+        @Override
+        public void read(Reads read) {
+            super.read(read);
+        }
+    }
+}
