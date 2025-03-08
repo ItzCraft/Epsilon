3412fe38dfa85c704c85e21306db5517ccb8790d
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 21:31:26 +0200

    Update TextShowup.java

--- src/epsilon/cutscenes/TextShowup.java
+++ src/epsilon/cutscenes/TextShowup.java
@@ -43,6 +43,6 @@ public class TextShowup extends CutsceneBase{
 
     @Override
     public String phaseToString() {
-        return "text" + " " + "[" + text + "]";
+        return "text_play" + " " + "[" + text + "]";
     }
 }
