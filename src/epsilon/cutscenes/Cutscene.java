97149b6ad667727e296d29272d7d408863aab586
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 12:06:26 +0200

    Create Cutscene.java

---
+++ src/epsilon/cutscenes/Cutscene.java
@@ -0,0 +1,19 @@
+package epsilon.cutscene;
+
+import arc.struct.Seq;
+
+public class Cutscene{
+    /*public static phaseCutscene(String tokens){
+         Seq<String> tokensArray = parseString(tokens);
+         String actionName = tokensArray.remove(0);
+         String[] args = tokensArray.toArray(String.class);
+         try{
+             return switch(actionName){
+                 case "text" -> new TextShowup(args);
+             };
+          }catch(Exception e){
+            ui.announce("Failed to parse action: " + tokens);
+            return new NullCutscene();
+         }
+    } */
+}
