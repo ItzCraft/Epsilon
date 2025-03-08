5167be55350a5b1e77f23c04c893b741dfd7db00
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 12:25:50 +0200

    Update TextShowup.java

--- src/epsilon/cutscenes/TextShowup.java
+++ src/epsilon/cutscenes/TextShowup.java
@@ -24,16 +24,14 @@ public class TextShowup extends CutsceneBase{
 
     @Override
     public void begin(){
-        public static void showDialog(){
-            Table table = new Table();
-            table.touchable = Touchable.disabled;
-            table.setFillParent(true);
-            table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
-            table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
-            // wanna make it through Flabel but idk how
-            table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(130f, 60f);
-            Core.scene.add(table);
-        }
+        Table table = new Table();
+        table.touchable = Touchable.disabled;
+        table.setFillParent(true);
+        table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
+        table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
+        // wanna make it through Flabel but idk how
+        table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(130f, 60f);
+        Core.scene.add(table);
     }
 
     @Override
