3b1fd479e4a59310ea2178fca3c7ffce73bbade0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 21:07:41 +0200

    Update CutsceneControl.java

--- src/epsilon/cutscenes/CutsceneControl.java
+++ src/epsilon/cutscenes/CutsceneControl.java
@@ -3,7 +3,7 @@ package epsilon.cutscenes;
 import arc.struct.Seq;
 
 public class CutsceneControl{
-    public Seq<CutsceneBus> cutsceneBuses = new Seq<>();
+    public static Seq<CutsceneBus> cutsceneBuses = new Seq<>();
 
     public static void addCutsceneBus(CutsceneBus bus){
         cutsceneBuses.add(bus);
