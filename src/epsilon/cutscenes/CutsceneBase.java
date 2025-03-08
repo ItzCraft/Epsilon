d69cf0b5d263442a4716b66895fe745a06fea89a
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 12:39:08 +0200

    Update CutsceneBase.java

--- src/epsilon/cutscenes/CutsceneBase.java
+++ src/epsilon/cutscenes/CutsceneBase.java
@@ -1,12 +1,38 @@
 package epsilon.cutscene;
 
-public abstract class CutsceneBase{
+import epsilon.util.TimeQueue;
 
-    public interface base{
-        void begin(){}
-    }
-    
+public abstract class CutsceneBase implements TimeQueue.Timed{
+        //lifeTimer timer
+        public float lifeTimer = 0;
+        //max time
+        public float maxTimer = 0; //Ticks
+
+        public Action(float duration){
+                this.maxTimer = duration;
+        }
+
+        @Override
+        public void update(){
+                if(lifeTimer < maxTimer){
+                        lifeTimer += Time.delta;
+                        act();
+                }
+        }
+
+        @Override
+        public boolean complete(){return lifeTimer >= maxTimer;}
+
+        @Override
+        public void begin(){}
+
+        @Override
+        public void end(){}
+
+        public void act(){}
+        public float progress(){return Mathf.clamp(lifeTimer / maxTimer);}
+
     public String phaseToString(){
         return "";
-    } 
+    }
 }
