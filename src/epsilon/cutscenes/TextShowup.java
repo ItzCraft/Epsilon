44c0efa28cffb40b52c1868efb8cb3c9061fc0c0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-08 13:28:14 +0200

    Update TextShowup.jav

--- src/epsilon/cutscenes/TextShowup.java
+++ src/epsilon/cutscenes/TextShowup.java
@@ -7,7 +7,7 @@ import arc.scene.event.Touchable;
 import arc.math.*;
 import mindustry.core.UI;
 import mindustry.ui.Styles;
-import epsilon.cutscene.CutsceneBase;
+import epsilon.cutscenes.CutsceneBase;
 
 import static arc.Core.bundle;
 import static mindustry.Vars.ui;
