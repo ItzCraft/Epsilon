ab0f1bde0c0745b5032752948f48c09f21b5c24f
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-09 07:55:42 +0200

    Update TextShowup.java

--- src/epsilon/cutscenes/TextShowup.java
+++ src/epsilon/cutscenes/TextShowup.java
@@ -34,8 +34,8 @@ public class TextShowup extends CutsceneBase{
         Table table = new Table();
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
-        table.actions(Actions.delay(5 * 0.8f), Actions.fadeOut(5 * 0.3f, Interp.fade), Actions.remove());
-        table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find("epsilon-frog")).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
+        table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
+        table.bottom().table(Styles.black3, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
         // wanna make it through Flabel but idk how
         table.bottom().table(Styles.black3, t -> t.margin(10f).add(text).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(130f, 60f);
         Core.scene.add(table);
