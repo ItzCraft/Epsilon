844282c57641bbba99ce3dddfe37e65196b14013
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-10 08:19:09 +0200

    Made the length of the ui (text) depend on the length of the text itself

--- src/epsilon/cutscenes/TextShowup.java
+++ src/epsilon/cutscenes/TextShowup.java
@@ -39,9 +39,9 @@ public class TextShowup extends CutsceneBase{
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
         table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
-        table.bottom().table(Styles.black5, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 115f);
+        table.bottom().table(Styles.black5, t -> t.margin(1).image(Core.atlas.find(unitIconName)).style(Styles.outlineLabel)).padLeft(-15f).padBottom(70f).size(65f, 80f);
         // wanna make it through Flabel but idk how
-        table.bottom().table(Styles.black5, t -> t.margin(10f).add(Core.bundle.get(text)).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(400f, 60f);
+        table.bottom().table(Styles.black5, t -> t.margin(10f).add(Core.bundle.get(text)).style(Styles.outlineLabel)).padRight(65f).padBottom(70f).size(Core.bundle.get(text).length() * 3.5f, 60f);
         Core.scene.add(table);
     }
   
