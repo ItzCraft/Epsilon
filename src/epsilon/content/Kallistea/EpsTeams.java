c2d1bae8a314f19715d7f735e57b3fa746f04f34
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-11 06:38:18 +0200

    Create EpsTeams.java

---
+++ src/epsilon/content/Kallistea/EpsTeams.java
@@ -0,0 +1,33 @@
+package epsilon.content.Kallistea;
+
+
+import arc.graphics.Color;
+import mindustry.game.Team;
+import epsilon.graphics.EpsPal;
+
+public class EpsTeams {
+    public static Team incers, ganieris, fenspor;
+
+    public static void load() {
+        ganieris = newTeam(42, "ganieris", EpsPal.ganierisTeam);
+        incers = newTeam(43, "incers", EpsPal.incersTeam);
+        //fenspor = newTeam(44, "fenspor", EpsPal.fenspor);
+    }
+
+    //modify any of 256 teams' properties
+    private static Team newTeam(int id, String name, Color color) {
+        Team team = Team.get(id);
+        team.name = name;
+        team.color.set(color);
+
+        team.palette[0] = color;
+        team.palette[1] = color.cpy().mul(0.75f);
+        team.palette[2] = color.cpy().mul(0.5f);
+
+        for(int i = 0; i < 3; i++){
+            team.palettei[i] = team.palette[i].rgba();
+        }
+
+        return team;
+    }
+}
