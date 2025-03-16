7fd1040be1bac4c0f7b02610aece3c23620d5fd1
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-16 08:32:35 +0200

    Create ColorableFloor.java
    Signed-off-by: ItzCraft <147850863+ItzCraft@users.noreply.github.com>

---
+++ src/epsilon/world/blocks/environment/ColorableFloor.java
@@ -0,0 +1,20 @@
+package epsilon.world.blocks.environment;
+
+import arc.*;
+import arc.graphics.*;
+import mindustry.world.blocks.environment.Floor;
+
+public class ColorableFloor extends Floor{
+    public Color color;
+
+    public ColorableFloor(String name, Color color){
+        this(name, 3, color);
+    }
+
+    public ColorableFloor(String name int variants, Color color){
+        super(name);
+        this.variants = variants;
+        this.mapColor.set(color);
+        this.useColor = true;
+    }
+}
