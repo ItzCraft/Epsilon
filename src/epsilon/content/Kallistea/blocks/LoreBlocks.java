b148d617661c04220c0fd2a8b29a4d86961c55f0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-23 11:12:55 +0200

    DialogBlock class have been deprecated

--- src/epsilon/content/Kallistea/blocks/LoreBlocks.java
+++ src/epsilon/content/Kallistea/blocks/LoreBlocks.java
@@ -10,7 +10,7 @@ import static mindustry.type.ItemStack.with;
 
 public class LoreBlocks{
     public static Block
-    testBlock1, dialogBlock1, cutsceneBlock;
+    testBlock1, cutsceneBlock;
 
     public static void load(){
         testBlock1 = new ImageBlock("test-block1"){{
@@ -21,12 +21,6 @@ public class LoreBlocks{
             dialogName = "Router of god";
         }};
 
-        dialogBlock1 = new DialogBlock("dialog-block1"){{
-            requirements(Category.distribution, with(KallisteaItems.calcite, 14884252));
-            health = 1;
-            size = 1;
-            text = "bebebe solo everyone";
-        }};
 
         cutsceneBlock = new CutsceneBlock("cutscene-block"){{
             requirements(Category.logic, with(KallisteaItems.gelionyte, 1448425269));
