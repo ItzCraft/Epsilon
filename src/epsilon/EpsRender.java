f08dd785a1598958903877f4fbb20037512d9510
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-26 11:45:09 +0200

    Update EpsRender.java

--- src/epsilon/EpsRender.java
+++ src/epsilon/EpsRender.java
@@ -1,5 +1,33 @@
 package epsilon;
 
-/*public class EpsRender implements Application{
+public class EpsRender extends Render{
+    public EpsRender(){
+           
+    }
 
-}*/
+    public void showLanding(LaunchAnimator landCore){
+        this.launchAnimator = landCore;
+        launching = false;
+        landTime = landCore.launchDuration();
+
+        landCore.beginLaunch(false);
+        camerascale = landCore.zoomLaunch();
+    }
+
+    public void showLaunch(LaunchAnimator landCore){
+        control.input.config.hideConfig();
+        control.input.planConfig.hide();
+        control.input.inv.hide();
+
+        this.launchAnimator = landCore;
+        launching = true;
+        landTime = landCore.launchDuration();
+
+        Music music = landCore.launchMusic();
+        music.stop();
+        music.play();
+        music.setVolume(settings.getInt("musicvol") / 100f);
+
+        landCore.beginLaunch(true);
+    }
+}
