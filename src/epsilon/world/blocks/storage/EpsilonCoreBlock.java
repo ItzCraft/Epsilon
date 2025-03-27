cc2550d3f557953a85394a36676d735daff5fa07
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-27 10:36:29 +0200

    Fuck this shit, no custom landing FX until V8
    Signed-off-by: ItzCraft <147850863+ItzCraft@users.noreply.github.com>

--- src/epsilon/world/blocks/storage/EpsilonCoreBlock.java
+++ src/epsilon/world/blocks/storage/EpsilonCoreBlock.java
@@ -1,43 +1,22 @@
 package epsilon.world.blocks.storage;
 
 import arc.Core;
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.math.geom.Geometry;
-import arc.scene.actions.Actions;
-import arc.scene.event.Touchable;
-import arc.scene.ui.Image;
-import arc.scene.ui.layout.Scl;
+import arc.graphics.Color;
+import arc.graphics.g2d.TextureRegion;
 import arc.util.Time;
 import arc.math.Mathf;
-import arc.util.Tmp;
 import epsilon.graphics.EpsPal;
-import epsilon.EpsRenderer;
-import mindustry.Vars;
-import mindustry.content.Fx;
-import mindustry.entities.Effect;
-import mindustry.graphics.*;
+import mindustry.gen.Building;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
 import mindustry.world.blocks.storage.CoreBlock;
-
-import static arc.Core.batch;
-import static epsilon.content.Kallistea.EpsFx.launchCore;
-import static mindustry.Vars.*;
 
 public class EpsilonCoreBlock extends CoreBlock{
     public Color[] glowColors = {Color.valueOf("00000000"), Color.coral, Color.valueOf("ff6161"), Color.pink, Color.acid, Color.sky, Color.red, EpsPal.ganierisTeam, EpsPal.incersTeam, EpsPal.fenspor};
     public TextureRegion glow;
     public float glowMag = 0.5f;
     public float glowScl = 10f;
-    public float landDuration = 160;
-    protected float cloudSeed, landParticleTimer;
-    public Effect launchEffect = launchCore;
-    public static final float cloudScaling = 1700f, cfinScl = -2f, cfinOffset = 0.3f, calphaFinOffset = 0.25f, cloudAlpha = 0.81f;
-    public static final float[] cloudAlphas = {0, 0.5f, 1f, 0.1f, 0, 0f};
-    public static float getColorAlpha(){
-        int abgr = Color.floatToIntColor(batch.getPackedColor());
-        return ((abgr & 0xff000000) >>> 24) / 255f;
-    }
 
     public EpsilonCoreBlock(String name){
         super(name);
@@ -49,233 +28,11 @@ public class EpsilonCoreBlock extends CoreBlock{
         glow = Core.atlas.find(name + "-glow");
         uiIcon = fullIcon = editorIcon = Core.atlas.find(name + "-full");
     }
-    public class EpsilonCoreBuild extends CoreBuild implements LaunchAnimator{
+    public class EpsilonCoreBuild extends CoreBuild{
         @Override
         public void draw(){
             super.draw();
             Drawf.additive(glow, team.id < 9 ? glowColors[team.id] : glowColors[1], 0.8f - glowMag + Mathf.absin(Time.time, glowScl, glowMag), x, y, 0f, Layer.blockAdditive);
-        }
-        @Override
-        public float launchDuration(){
-            return landDuration;
-        }
-
-        @Override
-        public float zoomLaunch() {
-            return 0;
-        }
-
-        /*
-        @Override
-        public void drawLaunch() {
-
-        }
-        */
-
-        @Override
-        public void beginLaunch(boolean launching){
-            cloudSeed = Mathf.random(1f);
-            if(launching){
-                Fx.coreLaunchConstruct.at(x, y, size);
-            }
-
-            if(!headless){
-                // Add fade-in and fade-out foreground when landing or launching.
-                if(renderer.isLaunching()){
-                    float margin = 30f;
-
-                    Image image = new Image();
-                    image.color.a = 0f;
-                    image.touchable = Touchable.disabled;
-                    image.setFillParent(true);
-                    image.actions(Actions.delay((launchDuration() - margin) / 60f), Actions.fadeIn(margin / 60f, Interp.pow2In), Actions.delay(6f / 60f), Actions.remove());
-                    image.update(() -> {
-                        image.toFront();
-                        ui.loadfrag.toFront();
-                        if(state.isMenu()){
-                            image.remove();
-                        }
-                    });
-                    Core.scene.add(image);
-                }else{
-                    Image image = new Image();
-                    image.color.a = 1f;
-                    image.touchable = Touchable.disabled;
-                    image.setFillParent(true);
-                    image.actions(Actions.fadeOut(35f / 60f), Actions.remove());
-                    image.update(() -> {
-                        image.toFront();
-                        ui.loadfrag.toFront();
-                        if(state.isMenu()){
-                            image.remove();
-                        }
-                    });
-                    Core.scene.add(image);
-
-                    Time.run(launchDuration(), () -> {
-                        launchEffect.at(this);
-                        Effect.shake(5f, 5f, this);
-                        thrusterTime = 1f;
-                    });
-                }
-            }
-        }
-        @Override
-        public void drawLaunch(){
-            var clouds = Core.assets.get("sprites/clouds.png", Texture.class);
-
-            float fin = EpsRenderer.getLandTimeIn();
-            float cameraScl = renderer.getDisplayScale();
-
-            float fout = 1f - fin;
-            float scl = Scl.scl(4f) / cameraScl;
-            float pfin = Interp.pow3Out.apply(fin), pf = Interp.pow2In.apply(fout);
-
-            //draw particles
-            Draw.color(Pal.lightTrail);
-            Angles.randLenVectors(1, pfin, 100, 800f * scl * pfin, (ax, ay, ffin, ffout) -> {
-                Lines.stroke(scl * ffin * pf * 3f);
-                Lines.lineAngle(x + ax, y + ay, Mathf.angle(ax, ay), (ffin * 20 + 1f) * scl);
-            });
-            Draw.color();
-
-            drawLanding(x, y);
-
-            Draw.color();
-            Draw.mixcol(Color.white, Interp.pow5In.apply(fout));
-
-            //accent tint indicating that the core was just constructed
-            if(renderer.isLaunching()){
-                float f = Mathf.clamp(1f - fout * 12f);
-                if(f > 0.001f){
-                    Draw.mixcol(Pal.accent, f);
-                }
-            }
-
-            //draw clouds
-            if(state.rules.cloudColor.a > 0.0001f){
-                float scaling = cloudScaling;
-                float sscl = Math.max(1f + Mathf.clamp(fin + cfinOffset) * cfinScl, 0f) * cameraScl;
-
-                Tmp.tr1.set(clouds);
-                Tmp.tr1.set(
-                        (Core.camera.position.x - Core.camera.width/2f * sscl) / scaling,
-                        (Core.camera.position.y - Core.camera.height/2f * sscl) / scaling,
-                        (Core.camera.position.x + Core.camera.width/2f * sscl) / scaling,
-                        (Core.camera.position.y + Core.camera.height/2f * sscl) / scaling);
-
-                Tmp.tr1.scroll(10f * cloudSeed, 10f * cloudSeed);
-
-                Draw.alpha(Mathf.sample(cloudAlphas, fin + calphaFinOffset) * cloudAlpha);
-                Draw.mixcol(state.rules.cloudColor, state.rules.cloudColor.a);
-                Draw.rect(Tmp.tr1, Core.camera.position.x, Core.camera.position.y, Core.camera.width, Core.camera.height);
-                Draw.reset();
-            }
-        }
-
-        public void drawLanding(float x, float y){
-            float fin = renderer.getLandTimeIn();
-            float fout = 1f - fin;
-
-            float scl = Scl.scl(4f) / renderer.getDisplayScale();
-            float shake = 0f;
-            float s = region.width * region.scl() * scl * 3.6f * Interp.pow2Out.apply(fout);
-            float rotation = Interp.pow2In.apply(fout) * 135f;
-            x += Mathf.range(shake);
-            y += Mathf.range(shake);
-            float thrustOpen = 0.25f;
-            float thrusterFrame = fin >= thrustOpen ? 1f : fin / thrustOpen;
-            float thrusterSize = Mathf.sample(thrusterSizes, fin);
-
-            //when launching, thrusters stay out the entire time.
-            if(renderer.isLaunching()){
-                Interp i = Interp.pow2Out;
-                thrusterFrame = i.apply(Mathf.clamp(fout*13f));
-                thrusterSize = i.apply(Mathf.clamp(fout*9f));
-            }
-
-            Draw.color(Pal.lightTrail);
-            //TODO spikier heat
-            Draw.rect("circle-shadow", x, y, s, s);
-
-            Draw.scl(scl);
-
-            //draw thruster flame
-            float strength = (1f + (size - 3)/2.5f) * scl * thrusterSize * (0.95f + Mathf.absin(2f, 0.1f));
-            float offset = (size - 3) * 3f * scl;
-
-            for(int i = 0; i < 4; i++){
-                Tmp.v1.trns(i * 90 + rotation, 1f);
-
-                Tmp.v1.setLength((size * tilesize/2f + 1f)*scl + strength*2f + offset);
-                Draw.color(team.color);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 6f * strength);
-
-                Tmp.v1.setLength((size * tilesize/2f + 1f)*scl + strength*0.5f + offset);
-                Draw.color(Color.white);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 3.5f * strength);
-            }
-
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
-
-            Drawf.spinSprite(region, x, y, rotation);
-
-            Draw.alpha(Interp.pow4In.apply(thrusterFrame));
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
-            Draw.alpha(1f);
-
-            if(teamRegions[team.id] == teamRegion) Draw.color(team.color);
-
-            Drawf.spinSprite(teamRegions[team.id], x, y, rotation);
-
-            Draw.color();
-            Draw.scl();
-            Draw.reset();
-        }
-
-        protected void drawLandingThrusters(float x, float y, float rotation, float frame){
-            float length = thrusterLength * (frame - 1f) - 1f/4f;
-            float alpha = Draw.getColorAlpha();
-
-            //two passes for consistent lighting
-            for(int j = 0; j < 2; j++){
-                for(int i = 0; i < 4; i++){
-                    var reg = i >= 2 ? thruster2 : thruster1;
-                    float rot = (i * 90) + rotation % 90f;
-                    Tmp.v1.trns(rot, length * Draw.xscl);
-
-                    //second pass applies extra layer of shading
-                    if(j == 1){
-                        Tmp.v1.rotate(-90f);
-                        Draw.alpha((rotation % 90f) / 90f * alpha);
-                        rot -= 90f;
-                        Draw.rect(reg, x + Tmp.v1.x, y + Tmp.v1.y, rot);
-                    }else{
-                        Draw.alpha(alpha);
-                        Draw.rect(reg, x + Tmp.v1.x, y + Tmp.v1.y, rot);
-                    }
-                }
-            }
-            Draw.alpha(1f);
-        }
-
-        public void drawThrusters(float frame){
-            float length = thrusterLength * (frame - 1f) - 1f/4f;
-            for(int i = 0; i < 4; i++){
-                var reg = i >= 2 ? thruster2 : thruster1;
-                float dx = Geometry.d4x[i] * length, dy = Geometry.d4y[i] * length;
-                Draw.rect(reg, x + dx, y + dy, i * 90);
-            }
-        }
-
-        @Override
-        public void endLaunch() {
-
-        }
-
-        @Override
-        public void updateLaunch() {
-
         }
     } 
 }
