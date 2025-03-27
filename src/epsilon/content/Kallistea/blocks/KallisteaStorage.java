b8d60ecf5dd3ef3fedd7a4099d4d5e1bb3196a09
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-27 11:52:23 +0200

    Update KallisteaStorage.java

--- src/epsilon/content/Kallistea/blocks/KallisteaStorage.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaStorage.java
@@ -12,14 +12,22 @@ import static mindustry.type.ItemStack.with;
 
 public class KallisteaStorage{
     public static Block
-    coreObscurity;
+    coreObscurityBroken, coreObscurity;
 
     public static void load(){
+        coreObscurityBroken = CoreBlock("core-obscurity-broken"){{
+            requirements(Category.effect, BuildVisibility.editorOnly, with(KallisteaItems.calcite, 120, KallisteaItems.gelionyte, 40));
+            health = 475;
+            size = 4;
+            itemCapacity = 828;
+            //unitType = EpsUnitTypes
+        }};
         coreObscurity = new EpsilonCoreBlock("core-obscurity"){{
             requirements(Category.effect, with(KallisteaItems.calcite, 120, KallisteaItems.gelionyte, 40));
             health = 475;
             size = 4;
             itemCapacity = 1450;
+            isFirstTier = true;
             //unitType = EpsUnitTypes
         }};
     }
