e7bc82b07807600263fa238f518b81a467187c8c
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-03-27 21:15:37 +0200

    Create ConsumeItemInfected.java
    Signed-off-by: ItzCraft <147850863+ItzCraft@users.noreply.github.com>

---
+++ src/epsilon/world/consumers/ConsumeItemInfected.java
@@ -0,0 +1,23 @@
+package mindustry.world.consumers;
+
+import mindustry.world.consumers.ConsumeItemEfficiency;
+import mindustry.type.*;
+import epsilon.world.EpsItem;
+
+public class ConsumeItemInfected extends ConsumeItemEfficiency{
+    public float minInfected;
+
+    public ConsumeItemInfected(float minInfected){
+        this.minInfected = minInfected;
+        filter = epsitem -> epsitem.infection >= this.minInfected;
+    }
+
+    public ConsumeItemInfected(){
+        this(0.25f);
+    }
+
+    @Override
+    public float itemEfficiencyMultiplier(EpsItem item){
+        return item.infection;
+    }
+}
