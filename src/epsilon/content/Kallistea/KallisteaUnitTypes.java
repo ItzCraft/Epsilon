ef236ff3f9d3f82b0e224604bb89350d896ab715
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-04-02 22:20:26 +0300

    Create KallisteaUnitTypes.java

---
+++ src/epsilon/content/Kallistea/KallisteaUnitTypes.java
@@ -0,0 +1,15 @@
+package epsilon.content.Kallistea;
+
+import arc.graphics.Color;
+import mindustry.ai.types.*;
+import mindustry.entities.bullet.*;
+import mindustry.type.*;
+import mindustry.type.weapons.*;
+
+import static mindustry.content.Fx.*;
+
+public class KallisteaUnitTypes{
+    public static UnitType
+    // core
+    penumbra;
+}
