15d371a39d88a885d7d7e27065f7be1b4ca130b0
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-04-03 22:37:24 +0300

    Update KallisteaProduction.java

--- src/epsilon/content/Kallistea/blocks/KallisteaProduction.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaProduction.java
@@ -18,7 +18,7 @@ public class KallisteaProduction{
         //drills
         pitMiningRig = new Drill("pit-mining-rig"){{
             requirements(Category.production, with(KallisteaItems.gelionyte, 25, KallisteaItems.calcite, 20));
-            health = 120;
+            health = 290;
             size = 3;
             drillTime = 1280;
             rotateSpeed = 1.25f;
@@ -27,7 +27,7 @@ public class KallisteaProduction{
         }};
         breakerDrill = new ConfigurableDrill("breaker-drill"){{
             requirements(Category.production, with(KallisteaItems.calcite, 145, KallisteaItems.quartz, 90, KallisteaItems.fylion, 70));
-            health = 300;
+            health = 590;
             size = 4;
             drillTime = 1075;
             rotateSpeed = 2;
