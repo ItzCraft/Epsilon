9f7cdb2ce6ff0b1d8b4d3a8a48461f79c0211bf4
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-04-19 09:08:22 +0300

    Update ColorCalc.java
    Signed-off-by: ItzCraft <147850863+ItzCraft@users.noreply.github.com>

--- src/epsilon/planet/ColorCalc.java
+++ src/epsilon/planet/ColorCalc.java
@@ -1,23 +1,19 @@
 package epsilon.planet;
+
 import arc.graphics.*;
 import arc.math.geom.*;
 import arc.util.*;
 import arc.util.noise.*;
 
+/*
+the code is from Omaloon.
+ **/
+
 public abstract class ColorCalc{
     public abstract @Nullable Color color(Vec3 pos, float height);
-    static class SphereColorCalc extends ColorCalc{
-        /**
-         * Position of the sphere relative to the planet.
-         */
+    public static class SphereColorCalc extends ColorCalc{
         public Vec3 pos;
-        /**
-         * Radius of the sphere.
-         */
         public float radius;
-        /**
-         * Color painted inside the sphere.
-         */
         public Color out;
 
         public SphereColorCalc(Vec3 pos, float radius, Color out){
@@ -34,37 +30,13 @@ public abstract class ColorCalc{
     }
 
     public static class NoiseColorCalc extends ColorCalc{
-        /**
-         * Offset for the noise sample relative to the planet. Values far away from the origin are reccomended.
-         */
         public Vec3 offset = new Vec3();
-        /**
-         * Noise seed.
-         */
         public int seed;
-        /**
-         * The amount of octves added to the noise.
-         */
         public double octaves = 1.0;
-        /**
-         * Intensity multiplier for each octave.
-         */
         public double persistence = 1.0;
-        /**
-         * Noise scale.
-         */
         public double scale = 1.0;
-        /**
-         * Noise magnitude.
-         */
         public float magnitude = 1;
-        /**
-         * Min and max treshold values of noise that paint will apply
-         */
         public float min = 0f, max = 1f;
-        /**
-         * Color painted based on the noise.
-         */
         public Color out = Color.white;
 
         @Override
