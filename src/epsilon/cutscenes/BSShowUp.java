c233781298d93931c8f113e6fd31c493514b68d6
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2025-04-19 21:33:45 +0300

    Update BSShowUp.java

--- src/epsilon/cutscenes/BSShowUp.java
+++ src/epsilon/cutscenes/BSShowUp.java
@@ -33,7 +33,7 @@ public class BSShowUp extends CutsceneBase{
         table.touchable = Touchable.disabled;
         table.setFillParent(true);
         table.actions(Actions.delay(duration * 0.8f), Actions.fadeOut(duration * 0.3f, Interp.fade), Actions.remove());
-        table.bottom().table(Styles.black5, t -> t.margin(1).image(Core.atlas.find("black-screen")).style(Styles.outlineLabel)).size(2500f, 2500f);
+        table.bottom().table(Styles.black5, t -> t.margin(1).image(Core.atlas.find("epsilon-black-screen")).style(Styles.outlineLabel)).size(2500f, 2500f);
         Core.scene.add(table);
     }
 
