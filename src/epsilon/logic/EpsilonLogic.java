9b1fe8c6af823c00770c41fbef88e7d0862e1f12
ItzCraft <tundukartem2397@gmail.com>
2025-06-13 21:57:55 +0300

    NEW WPROC FEATURE LET'S GOOOOO

--- src/epsilon/logic/EpsilonLogic.java
+++ src/epsilon/logic/EpsilonLogic.java
@@ -1,12 +1,19 @@
 package epsilon.logic;
 
-import mindustry.graphics.Pal;
+import arc.graphics.Color;
+import epsilon.logic.statements.PlayerChange;
+import mindustry.gen.LogicIO;
+import mindustry.logic.LAssembler;
 import mindustry.logic.LCategory;
 
 public class EpsilonLogic{
     public static LCategory epsilonCategory;
 
     public static void init(){
-        epsilonCategory = new LCategory("epsilonCategory", Pal.gray);
+        epsilonCategory = new LCategory("epsilon-category", Color.valueOf("c270bc"));
+
+        LAssembler.customParsers.put("playerchange", PlayerChange::new);
+
+        LogicIO.allStatements.addUnique(PlayerChange::new);
     }
 }
---
+++ src/epsilon/logic/instructions/PlayerChangeI.java
@@ -0,0 +1,27 @@
+package epsilon.logic.instructions;
+
+import mindustry.gen.Unit;
+import mindustry.logic.LExecutor;
+import mindustry.logic.LExecutor.LInstruction;
+import mindustry.logic.LVar;
+import mindustry.type.UnitType;
+
+import static mindustry.Vars.player;
+
+public class PlayerChangeI implements LInstruction {
+    public LVar unitVar;
+
+    public PlayerChangeI(LVar unitVar){
+        this.unitVar = unitVar;
+    }
+
+    public PlayerChangeI(){}
+
+    @Override
+    public void run(LExecutor exec){;
+        if(unitVar.obj() instanceof UnitType type){
+            Unit unit = type.spawn(player.team(), player.x, player.y, 90);
+            player.unit(unit);
+        }
+    }
+}
