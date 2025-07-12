01bbb9c2a6dc82b8f84578e57b032fa8333fde88
ItzCraft <tundukartem2397@gmail.com>
2025-07-12 16:29:08 +0300

    j

--- src/epsilon/world/Mission.java
+++ src/epsilon/world/Mission.java
@@ -1,18 +1,16 @@
 package epsilon.world;
 
-import mindustry.type.Item;
-import mindustry.world.meta.Stat;
+import mindustry.ctype.ContentType;
+import mindustry.ctype.UnlockableContent;
 
-public class Mission extends Item {
+public class Mission extends UnlockableContent {
     public boolean ARBITRASH;
     public Mission(String name){
         super(name);
     }
+
     @Override
-    public void setStats(){
-        stats.remove(Stat.charge);
-        stats.remove(Stat.explosiveness);
-        stats.remove(Stat.radioactivity);
-        stats.remove(Stat.flammability);
+    public ContentType getContentType(){
+        return ContentType.status;
     }
 }
