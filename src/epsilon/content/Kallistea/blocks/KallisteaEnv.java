83831391af5748c4492d2013041f24bc77c8e22a
Kev-Vily <146146416+Kev-Vily@users.noreply.github.com>
2025-10-17 21:10:25 +0700

    Hope this organized work is working

--- src/epsilon/content/Kallistea/blocks/KallisteaEnv.java
+++ src/epsilon/content/Kallistea/blocks/KallisteaEnv.java
@@ -25,14 +25,17 @@ public class KallisteaEnv {
             calciteOre, quartzOre,
 
             //purystal-biome
-            purystalErodedFloor, purystalFloor, purystalLightFloor,
-            purystalWall, purystalWallAlt, gelionyticWall,
-            purystalBoulder, gelionyteChunk, gelionyteCluster, buahanBush, buahanTree,
+            purystalErodedFloor, purystalFloor, purystalLightFloor, 
+			purystalWall, purystalWallAlt, purystalBoulder, 
+			gelionyticWall, gelionyteChunk, gelionyteCluster, 
 
             // eadstal biome
-            eadstonFloor, eadstonLightFloor, eadstonRoughFloor, fensporReinforcedFloor, fensporStrandsFloor, fensporVent,
-            eadstonWall, eadstonWallAlt, eadstonWallDeanytic, eadstalMagnetiteWall, eadstonWallFenspor,
-            eadstonBoulder, eadstonFensporBoulder, eadstalDeanyteCluster, buahanDeadBush, buahanFensporDeadBush, buahanDeadTree, 
+            eadstonFloor, eadstonLightFloor, eadstonRoughFloor, 
+            eadstonWall, eadstonWallAlt, eadstonBoulder, eadstalMagnetiteWall, eadstonWallDeanytic, eadstalDeanyteCluster, eadstonWallFenspor, eadstonFensporBoulder,
+
+			//buahan
+			fensporReinforcedFloor, fensporStrandsFloor, fensporVent,
+			buahanDeadBush, buahanFensporDeadBush, buahanDeadTree, buahanBush, buahanTree,
 
             // Merapora biome
             meraporaFloor, meraporaFensporFloor, meraporaWall, meraporaWallAlt, meraporaFensporWall, meraporaFensporWallAlt,
@@ -62,70 +65,60 @@ public class KallisteaEnv {
 
 
                 //PURYSTAL BIOME
-                // floors
                 purystalErodedFloor = new Floor("purystal-eroded-floor", 6);
                 purystalFloor = new Floor("purystal-floor", 6){{this.albedo = 0.35f;}};
                 purystalLightFloor = new Floor("purystal-light-floor", 6);
-
-                // walls
                 purystalWall = new StaticWall("purystal-wall"){{variants = 4;
                     purystalErodedFloor.asFloor().wall = this; purystalFloor.asFloor().wall = this; purystalLightFloor.asFloor().wall = this;
                 }};
                 purystalWallAlt = new StaticWall("purystal-wall-alt"){{variants = 4;
                     purystalErodedFloor.asFloor().wall = this; purystalFloor.asFloor().wall = this; purystalLightFloor.asFloor().wall = this;
                 }};
-                gelionyticWall = new StaticWall("gelionytic-wall"){{variants = 4;
+				purystalBoulder = new Prop("purystal-boulder"){{variants = 4; customShadow = true;
+                    purystalErodedFloor.asFloor().decoration = this; purystalFloor.asFloor().decoration = this; purystalLightFloor.asFloor().decoration = this;
+                }};          
+				gelionyticWall = new StaticWall("gelionytic-wall"){{variants = 4;
                     purystalErodedFloor.asFloor().wall = this; purystalFloor.asFloor().wall = this; purystalLightFloor.asFloor().wall = this;
                                 itemDrop = KallisteaItems.gelionyte;
                 }};
-
-                // decor, etc
-                purystalBoulder = new Prop("purystal-boulder"){{variants = 4; customShadow = true;
-                    purystalErodedFloor.asFloor().decoration = this; purystalFloor.asFloor().decoration = this; purystalLightFloor.asFloor().decoration = this;
-                }};          
                 gelionyteChunk = new BreakableTallBlock("gelionyte-chunk"){{requirements(Category.distribution, with(KallisteaItems.gelionyte, 24)); buildCostMultiplier = 6; variants = 3; clipSize = 148; shadowOffset = -0.68f;}};
                 gelionyteCluster = new TallBlock("gelionyte-cluster"){{variants = 2; clipSize = 148; shadowOffset = -0.68f; emitLight = true; lightColor = Color.valueOf("a86bd1"); lightRadius = 40f;}};
-                buahanBush = new Prop("buahan-bush"){{variants = 3; customShadow = true;}};
-                buahanTree = new TreeBlock("buahan-tree"){{variants = 2; clipSize = 200; shadowOffset = -1.53f;}};
-   
+
                 // EADSTON BIOME
-                // floors
                 eadstonFloor = new ColorableFloor("eadston-floor", 6, Color.valueOf("818a8c")){{color = Color.valueOf("818a8c");}};
                 eadstonLightFloor = new ColorableFloor("eadston-light-floor", 6, Color.valueOf("656c6e")){{color = Color.valueOf("656c6e");}};
                 eadstonRoughFloor = new ColorableFloor("eadston-rough-floor", 6, Color.valueOf("474e4f")){{color = Color.valueOf("474e4f");}};
-                fensporReinforcedFloor = new Floor("fenspor-reinforced-floor", 6);
-                fensporStrandsFloor = new Floor("fenspor-strands-floor", 6);
-                fensporVent = new SteamVent("fenspor-vent"){{variants= 2; parent = blendGroup = fensporReinforcedFloor = fensporStrandsFloor;
-                    attributes.set(infection, 1f);}};
-
-                // walls
-                eadstonWall = new StaticWall("eadston-wall"){{variants = 4;
+				eadstonWall = new StaticWall("eadston-wall"){{variants = 4;
                     eadstonFloor.asFloor().wall = this; eadstonLightFloor.asFloor().wall = this; eadstonRoughFloor.asFloor().wall = this;
                 }};
                 eadstonWallAlt = new StaticWall("eadston-wall-alt"){{variants = 4;
                     eadstonFloor.asFloor().wall = this; eadstonLightFloor.asFloor().wall = this; eadstonRoughFloor.asFloor().wall = this;
                 }};
-                eadstonWallDeanytic = new StaticWall("eadston-deanytic-wall"){{variants = 4;
-                    eadstonFloor.asFloor().wall = this; eadstonLightFloor.asFloor().wall = this; eadstonRoughFloor.asFloor().wall = this;
-                }};
-                eadstalMagnetiteWall = new StaticWall("eadstal-magnetite-wall"){{
-                itemDrop = KallisteaItems.magnetite;
-                variants = 4;}};
-                eadstonWallFenspor = new StaticWall("eadston-fenspor-wall"){{variants = 4;
-                    fensporReinforcedFloor.asFloor().wall = this; fensporStrandsFloor.asFloor().wall = this;
-                }};
-
-                // decor, etc
-                eadstonBoulder = new Prop("eadston-boulder"){{variants = 4; customShadow = true;
+				eadstonBoulder = new Prop("eadston-boulder"){{variants = 4; customShadow = true;
                     eadstonFloor.asFloor().decoration = this; eadstonLightFloor.asFloor().decoration = this; eadstonRoughFloor.asFloor().decoration = this;
                 }};
-                eadstonFensporBoulder = new Prop("eadston-fenspor-boulder"){{variants = 4; customShadow = true;
+				eadstalMagnetiteWall = new StaticWall("eadstal-magnetite-wall"){{
+                itemDrop = KallisteaItems.magnetite;
+                variants = 4;}};
+				eadstonWallDeanytic = new StaticWall("eadston-deanytic-wall"){{variants = 4;
+                    eadstonFloor.asFloor().wall = this; eadstonLightFloor.asFloor().wall = this; eadstonRoughFloor.asFloor().wall = this;
+                }};
+				eadstalDeanyteCluster = new TallBlock("eadstal-deanyte-cluster"){{variants = 2; clipSize = 148; shadowOffset = -0.68f;}};
+				eadstonWallFenspor = new StaticWall("eadston-fenspor-wall"){{variants = 4;
+                    fensporReinforcedFloor.asFloor().wall = this; fensporStrandsFloor.asFloor().wall = this;
+                }};
+				eadstonFensporBoulder = new Prop("eadston-fenspor-boulder"){{variants = 4; customShadow = true;
                     fensporReinforcedFloor.asFloor().decoration = this; fensporStrandsFloor.asFloor().decoration = this;
                 }};
-                eadstalDeanyteCluster = new TallBlock("eadstal-deanyte-cluster"){{variants = 2; clipSize = 148; shadowOffset = -0.68f;}};
+                fensporReinforcedFloor = new Floor("fenspor-reinforced-floor", 6);
+                fensporStrandsFloor = new Floor("fenspor-strands-floor", 6);
+                fensporVent = new SteamVent("fenspor-vent"){{variants= 2; parent = blendGroup = fensporReinforcedFloor = fensporStrandsFloor;
+                    attributes.set(infection, 1f);}};
                 buahanDeadBush = new Prop("buahan-dead-bush"){{variants = 3; customShadow = true;}};
                 buahanFensporDeadBush = new Prop("buahan-fenspor-dead-bush"){{variants = 3; customShadow = true;}};
                 buahanDeadTree = new TreeBlock("buahan-dead-tree"){{variants = 2; clipSize = 200; shadowOffset = -1.53f;}};
+				buahanBush = new Prop("buahan-bush"){{variants = 3; customShadow = true;}};
+                buahanTree = new TreeBlock("buahan-tree"){{variants = 2; clipSize = 200; shadowOffset = -1.53f;}};
 
                 // MERAPORA BIOME
                 // floors
